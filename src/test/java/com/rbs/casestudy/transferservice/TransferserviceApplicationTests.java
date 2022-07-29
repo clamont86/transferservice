bb1594cb569bff9412e935e052261a463eaeace1
Craig Lamont <biglam86@gmail.com>
2022-07-29 13:06:09 +0100

    initial commit

---
+++ src/test/java/com/rbs/casestudy/transferservice/TransferserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbs.casestudy.transferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
